7a4969ee242314816259ef510de3ddb8e5936289
a-palchikov <deemok@gmail.com>
2022-02-18 01:14:25 +0100

    Add compression support for tar exporter. TODO(dima): fork and update x/net/http2 instead of editing the vendor directory.

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ binaries: FORCE
 
 images: FORCE
 # moby/buildkit:local and moby/buildkit:local-rootless are created on Docker
-	hack/images local moby/buildkit
-	TARGET=rootless hack/images local moby/buildkit
+	PLATFORMS="linux/amd64" hack/images local moby/buildkit
+	#TARGET=rootless hack/images local moby/buildkit
 
 install: FORCE
 	mkdir -p $(DESTDIR)$(bindir)
--- exporter/tar/export.go
+++ exporter/tar/export.go
@@ -2,6 +2,7 @@ package local
 
 import (
 	"context"
+	"io"
 	"io/ioutil"
 	"os"
 	"strconv"
@@ -9,13 +10,18 @@ import (
 	"time"
 
 	"github.com/docker/docker/pkg/idtools"
+	"github.com/hashicorp/go-multierror"
+	"github.com/klauspost/compress/gzip"
+	"github.com/klauspost/compress/zstd"
 	"github.com/moby/buildkit/cache"
 	"github.com/moby/buildkit/exporter"
 	"github.com/moby/buildkit/session"
 	"github.com/moby/buildkit/session/filesync"
 	"github.com/moby/buildkit/snapshot"
+	"github.com/moby/buildkit/util/compression"
 	"github.com/moby/buildkit/util/progress"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"github.com/tonistiigi/fsutil"
 	fstypes "github.com/tonistiigi/fsutil/types"
 )
@@ -25,6 +31,9 @@ const (
 	// already found to use a non-distributable media type.
 	// When this option is not set, the exporter will change the media type of the layer to a distributable one.
 	preferNondistLayersKey = "prefer-nondist-layers"
+	keyLayerCompression    = "compression"
+	keyCompressionLevel    = "compression-level"
+	keyForceCompression    = "force-compression"
 )
 
 type Opt struct {
@@ -44,13 +53,45 @@ func New(opt Opt) (exporter.Exporter, error) {
 func (e *localExporter) Resolve(ctx context.Context, opt map[string]string) (exporter.ExporterInstance, error) {
 	li := &localExporterInstance{localExporter: e}
 
-	v, ok := opt[preferNondistLayersKey]
-	if ok {
-		b, err := strconv.ParseBool(v)
-		if err != nil {
-			return nil, errors.Wrapf(err, "non-bool value for %s: %s", preferNondistLayersKey, v)
+	for k, v := range opt {
+		switch k {
+		case keyLayerCompression:
+			switch v {
+			case "gzip":
+				li.layerCompression = compression.Gzip
+			case "estargz":
+				li.layerCompression = compression.EStargz
+			case "zstd":
+				li.layerCompression = compression.Zstd
+			case "uncompressed":
+				li.layerCompression = compression.Uncompressed
+			default:
+				return nil, errors.Errorf("unsupported layer compression type: %v", v)
+			}
+		case keyCompressionLevel:
+			ii, err := strconv.ParseInt(v, 10, 64)
+			if err != nil {
+				return nil, errors.Wrapf(err, "non-int value %s specified for %s", v, k)
+			}
+			v := int(ii)
+			li.compressionLevel = &v
+		case keyForceCompression:
+			if v == "" {
+				li.forceCompression = true
+				continue
+			}
+			b, err := strconv.ParseBool(v)
+			if err != nil {
+				return nil, errors.Wrapf(err, "non-bool value %v specified for %s", v, k)
+			}
+			li.forceCompression = b
+		case preferNondistLayersKey:
+			b, err := strconv.ParseBool(v)
+			if err != nil {
+				return nil, errors.Wrapf(err, "non-bool value %v specified for %s", v, k)
+			}
+			li.preferNonDist = b
 		}
-		li.preferNonDist = b
 	}
 
 	return li, nil
@@ -58,7 +99,10 @@ func (e *localExporter) Resolve(ctx context.Context, opt map[string]string) (exp
 
 type localExporterInstance struct {
 	*localExporter
-	preferNonDist bool
+	preferNonDist    bool
+	layerCompression compression.Type
+	compressionLevel *int
+	forceCompression bool
 }
 
 func (e *localExporterInstance) Name() string {
@@ -168,12 +212,100 @@ func (e *localExporterInstance) Export(ctx context.Context, inp exporter.Source,
 	if err != nil {
 		return nil, err
 	}
+
+	comp := e.compression()
+	switch comp.Type {
+	case compression.Zstd:
+		logrus.Info("Compressing with zstd")
+		w, err = zstdWriter(comp, w)
+		if err != nil {
+			return nil, err
+		}
+	case compression.Gzip:
+		logrus.Info("Compressing with gzip")
+		w, err = gzipWriter(comp, w)
+		if err != nil {
+			return nil, err
+		}
+	case compression.Uncompressed:
+		logrus.Info("Storing uncompressed")
+	}
+
 	report := oneOffProgress(ctx, "sending tarball")
 	if err := fsutil.WriteTar(ctx, fs, w); err != nil {
 		w.Close()
 		return nil, report(err)
 	}
 	return nil, report(w.Close())
+}
+
+func (e *localExporterInstance) compression() compression.Config {
+	c := compression.New(e.layerCompression).SetForce(e.forceCompression)
+	if e.compressionLevel != nil {
+		c = c.SetLevel(*e.compressionLevel)
+	}
+	return c
+}
+
+func zstdWriter(comp compression.Config, wc io.WriteCloser) (io.WriteCloser, error) {
+	level := zstd.SpeedDefault
+	if comp.Level != nil {
+		level = toZstdEncoderLevel(*comp.Level)
+	}
+	zw, err := zstd.NewWriter(wc, zstd.WithEncoderLevel(level))
+	if err != nil {
+		return nil, err
+	}
+	return multiCloser{
+		Writer:  zw,
+		closers: []io.Closer{zw, wc},
+	}, nil
+
+}
+
+func toZstdEncoderLevel(level int) zstd.EncoderLevel {
+	// map zstd compression levels to go-zstd levels
+	// once we also have c based implementation move this to helper pkg
+	if level < 0 {
+		return zstd.SpeedDefault
+	} else if level < 3 {
+		return zstd.SpeedFastest
+	} else if level < 7 {
+		return zstd.SpeedDefault
+	} else if level < 9 {
+		return zstd.SpeedBetterCompression
+	}
+	return zstd.SpeedBestCompression
+}
+
+func gzipWriter(comp compression.Config, wc io.WriteCloser) (io.WriteCloser, error) {
+	level := gzip.DefaultCompression
+	if comp.Level != nil {
+		level = *comp.Level
+	}
+	gz, err := gzip.NewWriterLevel(wc, level)
+	if err != nil {
+		return nil, err
+	}
+	return multiCloser{
+		Writer:  gz,
+		closers: []io.Closer{gz, wc},
+	}, nil
+}
+
+// Implements io.Closer
+func (r multiCloser) Close() (rerr error) {
+	for _, c := range r.closers {
+		if err := c.Close(); err != nil {
+			rerr = multierror.Append(rerr, err).ErrorOrNil()
+		}
+	}
+	return rerr
+}
+
+type multiCloser struct {
+	io.Writer
+	closers []io.Closer
 }
 
 func oneOffProgress(ctx context.Context, id string) func(err error) error {
